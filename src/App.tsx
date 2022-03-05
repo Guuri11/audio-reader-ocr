51c967fadd90cb1d847cb89f6b470a26a03f3bf4
Sergio Gurillo Corral <sergio.gurillo11@gmail.com>
2022-03-06 00:03:22 +0100

    ocr rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import Tesseract from 'tesseract.js';
 import './App.css';
 
 function App() {
+
+  const video = document.querySelector('video');
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const setUp = async () => {
+    if (video) {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true })
+      video.srcObject = stream;
+      video.addEventListener('playing', async () => {
+        const worker = Tesseract.createWorker();
+        await worker.load();
+        await worker.loadLanguage('es');
+        await worker.initialize('es');
+
+        const canvas = document.createElement('canvas');
+        canvas.width = video.width;
+        canvas.height = video.height;
+        
+        document.addEventListener('keypress', async (e) => {
+          if (e.code === 'Space') {
+            canvas.getContext('2d')?.drawImage(video, 0, 0, video.width, video.height);
+            const recognizeObj = await worker.recognize(canvas);
+            speechSynthesis.speak(new SpeechSynthesisUtterance(recognizeObj.data.text.replace(/\s/g, ' ')))
+            
+          }
+        })
+
+      })
+    }
+  }
+
+  useEffect(() => {
+    setUp();
+  }, [setUp])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Audio Reader with OCR</h1>
+      <video width="720" height="560" autoPlay muted />
+      <p>Show a text to the cam and press Start. Then wait for the Speech Synthesis API for speak</p>
     </div>
   );
 }
